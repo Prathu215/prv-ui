5976ddb0504cede8610d9b2ce2f7f711e7eda9a2
Atchutha <av@Atchuthas-Air.lan>
2022-07-15 19:24:38 -0500

    feat:PRV-home profile page,css changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './components/content/content.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: ContentComponent },
+  { path: 'gallery', component: GalleryComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
