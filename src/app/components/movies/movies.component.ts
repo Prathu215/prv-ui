fbe3f142f429f0ffe3d0ae721381d1be0e41e025
Prathyusha Vinukonda <atchutha@Prathyushas-MBP.lan>
2022-09-12 18:31:21 -0500

    feat:movies form elements

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpAppService } from 'src/app/services/http.service';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-movies',
@@ -8,10 +9,35 @@ import { HttpAppService } from 'src/app/services/http.service';
 })
 export class MoviesComponent implements OnInit {
 public movieList : any;
-  constructor(private httpAppService: HttpAppService) {}
+public movieFilter: FormGroup;
+ratingArray =[
+"Blockbuster",
+"Hit",
+"Average",
+"Below Average",
+"Flop"
+]
+
+typeArray= [
+  "Cover Songs",
+  "Web Series",
+  "Short Films",
+  "Movies"
+]
+
+  constructor(private fb: FormBuilder,
+    private httpAppService: HttpAppService) {}
 
   ngOnInit(): void {
     this.getMovies();
+
+    this.movieFilter = this.fb.group({
+      title: ["", [Validators.required]],
+      rating: [null, [Validators.required]],
+      type: [null,[Validators.required]],
+      release_date: ["", [Validators.required]],
+      star_rating: ["", [Validators.required]],
+    });
   }
   getMovies() {
     this.httpAppService.getMovies().subscribe((response: any) => {
@@ -23,4 +49,15 @@ public movieList : any;
     return desc.substring(0, 100) + "...";
   }
 
+  filter(model){
+    console.log(model);
+  }
+
+  get form() { 
+    return this.movieFilter.controls; 
+  }
+
 }
+
+
+
